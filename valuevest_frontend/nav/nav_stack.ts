fa33150ef5bb1c89aa0c0fb28af4334ab8fdcb77
derpyburpz <jake0003@e.ntu.edu.sg>
2024-04-04 13:57:44 +0800

    Updated nav_stack.ts

--- valuevest_frontend/nav/nav_stack.ts
+++ valuevest_frontend/nav/nav_stack.ts
@@ -7,17 +7,10 @@ export type RootStackParamList = {
   Profile: undefined;
   Search: undefined;
   Login: undefined;
-  StockInfo: {
-    ticker: string;
-  };
+  StockInfo: { ticker: string; };
   AuthSplash: undefined;
-  AuthStack: {
-      screen: string;
-  };
-  AppStack: {
-    screen: string;
-  };
+  AuthStack: { screen: string; };
+  AppStack: { screen: string; };
   App: undefined;
-
   NotAuthenticated: undefined;
 };
